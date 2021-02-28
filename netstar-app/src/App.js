d789e767b980a6639afefb86ccee4f330013f65d
Jeet Khondker <jeetzhkhondker@gmail.com>
2021-02-28 18:20:22 +0900

    feature: Used 'Row' Component in Main Application

--- netstar-app/src/App.js
+++ netstar-app/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+import Row from "./components/Row"
+
+import requests from "./api/requests"
+
 function App() {
   return (
     <div className="App">
-      
+      <Row title="ORIGINALS" fetchURL={ requests.fetchOriginals } />
+      <Row title="TRENDING NOW" fetchURL={ requests.fetchTrending } />
+      <Row title="TOP RATED" fetchURL={ requests.fetchTopRated } />
+      <Row title="ACTION MOVIES" fetchURL={ requests.fetchActionMovies } />
+      <Row title="COMEDY MOVIES" fetchURL={ requests.fetchComedyMovies } />
+      <Row title="HORROR MOVIES" fetchURL={ requests.fetchHorrorMovies } />
+      <Row title="ROMANCE MOVIES" fetchURL={ requests.fetchRomanceMovies } />
+      <Row title="DOCUMENTARIES" fetchURL={ requests.fetchDocumentaries } />
     </div>
   );
 }
