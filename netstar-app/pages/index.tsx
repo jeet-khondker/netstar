33200e62abcf7ea059dc34c5ef1529d593596557
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-10 08:24:41 +0900

    fix : 新規ブラウザアイコンの設定

--- netstar-app/pages/index.tsx
+++ netstar-app/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       <Head>
         <title>NetStar - 動画Webストア</title>
         <meta name="description" content="NetStar - 動画Webストア" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/netstar-browser.ico" />
       </Head>
     </div>
   );
