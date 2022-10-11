7a8ef7ad321a425e9a8b304356561cee8b438591
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-11 21:09:28 +0900

    feature : ヘッダーコンポーネントはindexファイルに追加

--- netstar-app/pages/index.tsx
+++ netstar-app/pages/index.tsx
@@ -1,15 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>NetStar - 動画Webストア</title>
         <meta name="description" content="NetStar - 動画Webストア" />
         <link rel="icon" href="/netstar-browser.ico" />
       </Head>
+      {/* Header */}
+      <Header />
+      {/* Main Contents */}
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row 1 */}
+          {/* Row 2 */}
+          {/* Row 3 */}
+          {/* Row n */}
+        </section>
+      </main>
+      {/* Popup Dialog */}
     </div>
   );
 };
