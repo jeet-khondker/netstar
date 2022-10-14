4d270ebeb192943dc07b60fb29ea6cfaa40abd78
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-15 08:03:36 +0900

    chore : TMDB APIから動画の画像を取得ため、ドメインをNextJSの設定ファイルに追加

--- netstar-app/next.config.js
+++ netstar-app/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // Reference URL : https://nextjs.org/docs/messages/next-image-unconfigured-host
+  images: {
+    domains: ['image.tmdb.org'],
+  },
 }
 
 module.exports = nextConfig
