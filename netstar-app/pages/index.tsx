fd711e0692ba29b8374ac8717334f96f8e8ea0de
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-15 08:36:30 +0900

    feature : SSRとバナーコンポーネントの追加

--- netstar-app/pages/index.tsx
+++ netstar-app/pages/index.tsx
@@ -1,8 +1,58 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
-const Home: NextPage = () => {
+import requests from "../api/requests";
+
+import type { PropsTypes } from "../types/movie";
+import type { GetServerSideProps } from "next";
+
+// Server Side Rendering (SSR)
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const [
+    originals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchOriginals).then((response) => response.json()),
+    fetch(requests.fetchTrending).then((response) => response.json()),
+    fetch(requests.fetchTopRated).then((response) => response.json()),
+    fetch(requests.fetchActionMovies).then((response) => response.json()),
+    fetch(requests.fetchComedyMovies).then((response) => response.json()),
+    fetch(requests.fetchHorrorMovies).then((response) => response.json()),
+    fetch(requests.fetchRomanceMovies).then((response) => response.json()),
+    fetch(requests.fetchDocumentaries).then((response) => response.json()),
+  ]);
+  return {
+    props: {
+      originals: originals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  };
+};
+
+const Home = ({
+  originals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: PropsTypes) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -13,8 +63,9 @@ const Home: NextPage = () => {
       {/* Header */}
       <Header />
       {/* Main Contents */}
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         {/* Banner */}
+        <Banner originals={originals} />
         <section>
           {/* Row 1 */}
           {/* Row 2 */}
