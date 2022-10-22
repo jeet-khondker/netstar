1bf16f271d52940b387e3dd23760ce42381ee7d7
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-22 20:21:16 +0900

    feature : ユーザーログアウトボタンの作成、ユーザーログアウト処理の追加

--- netstar-app/components/Header.tsx
+++ netstar-app/components/Header.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 const Header = () => {
   // Header Component Changes while Scrolling
@@ -23,6 +24,8 @@ const Header = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const { logout } = useAuth();
 
   return (
     <header className={`${isScrolled && "bg-[#111111]"}`}>
@@ -71,6 +74,21 @@ const Header = () => {
             height={25}
           />
         </Link>
+        <svg
+          onClick={logout}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+          />
+        </svg>
       </nav>
     </header>
   );
