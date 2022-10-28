06ff79ed3c1c5220c8be28e4d2ac8f1af7e6402f
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-28 16:50:17 +0900

    feature : 「プレー」ボタンの削除

--- netstar-app/components/Banner.tsx
+++ netstar-app/components/Banner.tsx
@@ -41,23 +41,6 @@ const Banner = ({ originals }: originalsType) => {
         {movie?.overview}
       </p>
       <section className="flex space-x-3">
-        <button className="bannerButton bg-white text-black">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-4 h-4 text-black md:h-7 md:w-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-            />
-          </svg>
-          プレー
-        </button>
         <button
           className="bannerButton bg-[gray]/70"
           onClick={() => {
