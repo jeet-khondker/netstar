5b7f4845d826e331d1f3c12d990925cf1949c381
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-30 15:37:40 +0900

    feature : 「BASE_URL」は映画constantとして追加

--- netstar-app/constants/movie-constants.ts
+++ netstar-app/constants/movie-constants.ts
@@ -1,3 +1,4 @@
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/"
 export const THUMBNAIL_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 export const TOP_BACKGROUND_IMAGE_COVER_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/757ab38f-5d08-40bc-b3eb-eaba63ed8203/93c34f94-56c8-40a7-8b2e-b4aac6427977/GB-en-20210125-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+export const BASE_URL = "https://api.themoviedb.org/3/";
