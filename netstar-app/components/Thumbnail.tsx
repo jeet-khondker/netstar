e6ce02c5158435071028e0a26d46e774b16070ec
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-30 15:52:33 +0900

    feature : State管理ため、映画のThumbnailにrecoilStateの追加

--- netstar-app/components/Thumbnail.tsx
+++ netstar-app/components/Thumbnail.tsx
@@ -3,9 +3,21 @@ import { MovieDetailType } from "../types/movie";
 
 import { THUMBNAIL_IMAGE_BASE_URL } from "../constants/movie-constants";
 
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
+
 const Thumbnail = ({ movie }: MovieDetailType) => {
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+  const [showModal, setShowModal] = useRecoilState(modalState);
+
   return (
-    <section className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+    <section
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        setCurrentMovie(movie);
+        setShowModal(true);
+      }}
+    >
       <Image
         src={`${THUMBNAIL_IMAGE_BASE_URL}${
           movie.backdrop_path || movie.poster_path
