8832f7d23d88ebfc428b57fd9ef034307acdb58b
Jeet Khondker <jeetzhkhondker@gmail.com>
2022-10-31 15:18:54 +0900

    fix : ダイヤログの修正

--- netstar-app/components/Modal.tsx
+++ netstar-app/components/Modal.tsx
@@ -105,40 +105,6 @@ const Modal = () => {
           )}
 
           <section className="absolute bottom-10 flex w-full items-center justify-between px-10">
-            <section className="flex space-x-2">
-              {/* Add To List Button */}
-              <button className="modalButton">
-                {addedToList ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-7 h-7"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-7 h-7"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 4.5v15m7.5-7.5h-15"
-                    />
-                  </svg>
-                )}
-              </button>
-            </section>
             <button className="modalButton" onClick={() => setMuted(!muted)}>
               {muted ? (
                 <svg
